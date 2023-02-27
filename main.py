e0f61cf1d0708a13ac20ebc97457404ad041c941
mnstmoney.eth <41440448+DavilkaS@users.noreply.github.com>
2023-02-27 20:29:26 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import aiohttp
+import asyncio
+import time
+url = 'https://fapi.binance.com/fapi/v1/ticker/bookTicker?symbol=ETHUSDT'
+
+
+class BadResponseStatus(Exception):
+    """Ошибка при парсинге биржи"""
+    pass
+
+
+async def parsing_binance():
+    """Запрос на тикер фьючерсов Binance"""
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                res = await resp.json()
+                return res['bidPrice']
+            else:
+                raise BadResponseStatus
+
+
+def check_changes_binance(sleep_time=3600):
+    """Подсчёт изменений в процентном соотношении"""
+    first_fetch = float(asyncio.run(parsing_binance()))
+    print(first_fetch)
+    time.sleep(sleep_time)
+    second_fetch = float(asyncio.run(parsing_binance()))
+    print(second_fetch)
+    result = abs(float((second_fetch - first_fetch)/first_fetch)*100)
+    if result >= 1:
+        return result
+    else:
+        return 'Курс почти на том же месте'
+
+
+if __name__ == "__main__":
+    print(check_changes_binance(sleep_time=3600))
